a19a21d87c9a385ac584956e360cba288c422879
subbusainath <subbusainath@outlook.com>
2020-08-16 12:56:31 +0530

    Added created cartItem dropdown and created Selector

--- src/components/CollectionsPreview/CollectionsPreview.component.jsx
+++ src/components/CollectionsPreview/CollectionsPreview.component.jsx
@@ -3,19 +3,18 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './CollectionsPreview.Styles.scss';
 
 
-const CollectionPreview = ({ title , items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{ title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-            items
-            .filter((item,idx) => idx < 4)
-            .map(({id, ...otherItemsProps}) => (
-            <CollectionItem key={id} {...otherItemsProps}/>
-            ))
-            }
-        </div>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-);
+  );
+  
 
 export default CollectionPreview;
--- src/pages/shopPage/shop.data.js
+++ src/pages/shopPage/shop.data.js
@@ -1,5 +1,5 @@
 const SHOP_DATA = [
-{
+  {
     id: 1,
     title: 'Hats',
     routeName: 'hats',
@@ -244,7 +244,6 @@ const SHOP_DATA = [
       }
     ]
   }
-
 ];
 
 export default SHOP_DATA;
